fc226a556174ce592ef327f51219f34043fd6682
Rob Penn <rob.penn@genovo.co.uk>
2020-06-18 13:07:18 +0100

    Chapter 29

---
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
